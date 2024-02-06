ad1096fb4e186b70ced78783b1639c324203ed8f
David Lee <luckydavid914@gmail.com>
2024-02-06 10:18:43 -0800

    Adding link from backend to frontend

--- model/users.py
+++ model/users.py
@@ -3,16 +3,11 @@ from random import randrange
 from datetime import date
 import os, base64
 import json
-
 from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
 ''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
-
 # Define the Post class to manage actions in 'posts' table,  with a relationship to 'users' table
-
 class User(db.Model):
     __tablename__ = 'users'  # table name is plural, class name is singular
     # Define the User schema with "vars" from object
@@ -21,16 +16,13 @@ class User(db.Model):
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
     _role = db.Column(db.String(255))
-    
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
-
     # constructor of a User object, initializes the instance variables within object (self)
     def __init__(self, name, uid, password="123qwerty", role="default"):
-        self._name = name    # variables with self prefix become part of the object, 
+        self._name = name    # variables with self prefix become part of the object,
         self._uid = uid
         self.set_password(password)
         self._role = role
-
     # a name getter method, extracts name from object
     @property
     def role(self):
@@ -41,49 +33,38 @@ class User(db.Model):
     @property
     def name(self):
         return self._name
-    
     # a setter function, allows name to be updated after initial object creation
     @name.setter
     def name(self, name):
         self._name = name
-    
     # a getter method, extracts email from object
     @property
     def uid(self):
         return self._uid
-    
     # a setter function, allows name to be updated after initial object creation
     @uid.setter
     def uid(self, uid):
         self._uid = uid
-        
     # check if uid parameter matches user id in object, return boolean
     def is_uid(self, uid):
         return self._uid == uid
-    
     @property
     def password(self):
         return self._password[0:10] + "..." # because of security only show 1st characters
-
     # update password, this is conventional setter
     def set_password(self, password):
         """Create a hashed password."""
         self._password = generate_password_hash(password, "pbkdf2:sha256", salt_length=10)
-
     # check password parameter versus stored/encrypted password
     def is_password(self, password):
         """Check against hashed password."""
         result = check_password_hash(self._password, password)
         return result
-    
     # dob property is returned as string, to avoid unfriendly outcomes
-
-    
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
     def __str__(self):
         return json.dumps(self.read())
-
     # CRUD create/add a new record to the table
     # returns self or None on error
     def create(self):
@@ -95,7 +76,6 @@ class User(db.Model):
         except IntegrityError:
             db.session.remove()
             return None
-
     # CRUD read converts self to dictionary
     # returns dictionary
     def read(self):
@@ -105,7 +85,6 @@ class User(db.Model):
             "uid": self.uid,
             "role": self.role
         }
-
     # CRUD update: updates user name, password, phone
     # returns self
     def update(self, name="", uid="", password="", role="default"):
@@ -120,18 +99,13 @@ class User(db.Model):
             self.role = role
         db.session.commit()
         return self
-
     # CRUD delete: remove self
     # None
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return None
-
-
 """Database Creation and Testing """
-
-
 # Builds working data for testing
 def initUsers():
     with app.app_context():
@@ -144,7 +118,6 @@ def initUsers():
         u4 = User(name='Grace Hopper', uid='hop', password='123hop')
         u5 = User(name='Admin', uid='root', password='root', role="admin")
         users = [u1, u2, u3, u4, u5]
-
         """Builds sample user/note(s) data"""
         for user in users:
             try:
