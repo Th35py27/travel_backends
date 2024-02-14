a8c3c8e4a79f4aed057ff2446e7e3c1143e98e8f
7mwang <matthewxpwang@gmail.com>
2024-02-14 11:53:40 -0800

    cookie domain

--- api/user.py
+++ api/user.py
@@ -121,7 +121,7 @@ class UserAPI:
                             algorithm="HS256"
                         )
                         resp = Response("Authentication for %s successful" % (user._uid))
-                        resp.set_cookie(key="jwt", value=token, max_age=3600, secure=True, samesite='None', path='/', httponly=False)
+                        resp.set_cookie(key="jwt", value=token, max_age=3600, secure=True, samesite='None', path='/', httponly=False, domain="davidl0914.github.io")
                         print(resp.headers)
                         return resp
                     except Exception as e:
